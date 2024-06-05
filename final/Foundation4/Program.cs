5a936ab842caad2305cfab77dc22cd6f037b32d8
Daniel Delgado <daniel.delgado1990@gmail.com>
2024-06-05 17:13:10 -0600

    Created classes and added code for Foundation 4 final.

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        var activities = new List<Activity>
+        {
+            new Running(new DateTime(2024, 6, 1), 30, 3),
+            new Cycling(new DateTime(2024, 6, 2), 45, 10),
+            new Swimming(new DateTime(2024, 6, 3), 60, 20)
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
