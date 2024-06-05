dd77707ab105238358889600d1e1c175e6695d4a
Daniel Delgado <daniel.delgado1990@gmail.com>
2024-06-04 21:17:37 -0600

    Created the classes and Foundation 2 code for final.

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        Order order1 = new Order();
+        order1.AddProduct("Apples", "AAA1", 2, 5);
+        order1.AddProduct("Oranges", "OOO1", 3, 4);
+        order1.SetCustomer("Michael Jordon", "1901 W Madison St", "Chicago", "IL", "60612", "USA");
+
+        Order order2 = new Order();
+        order2.AddProduct("Apples", "AAA1", 2, 6);
+        order2.AddProduct("Oranges", "OOO1", 3, 5);
+        order2.AddProduct("Starwberries", "SSS1", 5, 4);
+        order2.SetCustomer("John Stockton", "301 S Temple", "Salt Lake City", "UT", "84101",  "UK");
+
+        Console.WriteLine($"Order1 Total Cost: ${order1.GetTotalCost()}");
+        order1.DisplayPackingLabel();
+        order1.DisplayShippingLabel();
+        Console.WriteLine();
+
+        Console.WriteLine($"Order2 Total Cost: ${order2.GetTotalCost()}");
+        order2.DisplayPackingLabel();
+        order2.DisplayShippingLabel();
+        Console.WriteLine();
+
     }
 }
