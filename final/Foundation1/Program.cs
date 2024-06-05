9b7d8a997d4446ea26084a0fd00f62f7dd2b32f0
Daniel Delgado <daniel.delgado1990@gmail.com>
2024-06-04 21:03:07 -0600

    Created Final Abstraction program with classes

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        List<Video> videos = new List<Video>();
+
+        Video video1 = new Video("ABC Video", "Jack Black", 260);
+        video1.AddComment("Alpha", "Great video!");
+        video1.AddComment("Bravo", "Made my day!.");
+        video1.AddComment("Charlie", "Very informative!");
+
+        Video video2 = new Video("1-10 Numbers", "Mary Poppins", 100);
+        video2.AddComment("Delta", "Fun way to learn.");
+        video2.AddComment("Echo", "My kids love this.");
+        video2.AddComment("Foxtrot", "Go to 100!");
+
+        videos.Add(video1);
+        videos.Add(video2);
+        
+        foreach (var video in videos)
+        {
+            video.DisplayVideoDetails();
+        }
     }
 }
